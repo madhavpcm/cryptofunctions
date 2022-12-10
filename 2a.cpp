c13b4480dd69b4e6560bfc0b2cce0b072a3ba0dc
C M Madhav Prabhu <dpmadhav@hotmail.com>
2022-12-10 23:51:39 +0530

    Des working

--- 2a.cpp
+++ 2a.cpp
@@ -1,5 +1,6 @@
 #include <Eigen/Dense>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <vector>
 
@@ -87,21 +88,28 @@ private:
         {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}}}};
 
 public:
-  std::vector<int> initial_permutation(std::vector<int> &input);
-  std::vector<int> final_permutation(std::vector<int> &input);
-  std::vector<int> expand32_48(std::vector<int> &input);
-  std::vector<int> straight32_32(std::vector<int> &input);
-  std::vector<int> XoR(std::vector<int> &a, std::vector<int> &b);
-  std::vector<int> encrypt(std::vector<int> &plainText,
-                           std::vector<std::vector<int>> &roundKeys);
-  void round_mixer(std::vector<int> &leftBlock, std::vector<int> &rightBlock,
-                   std::vector<int> &roundKey);
-  std::vector<int> round_function(std::vector<int> &input,
-                                  std::vector<int> &roundKey);
-  std::vector<int> substitute(std::vector<int> &input);
-  std::vector<std::vector<int>> keygen();
-  uint64_t DES::drop_parity_bits(uint64_t key);
+  DES(uint64_t key);
+  std::vector<uint64_t> round_keys;
+  uint64_t initial_permutation(uint64_t &input);
+  uint64_t final_permutation(uint64_t &input);
+
+  uint64_t encrypt(uint64_t &plainText);
+  uint64_t decrypt(uint64_t &plainText);
+
+  void round_mixer(uint32_t &leftBlock, uint32_t &rightBlock,
+                   uint64_t &roundKey);
+  uint64_t round_function(uint32_t &input, uint64_t &roundKey);
+  uint64_t expand32_48(uint32_t &input);
+  uint32_t substitute(uint64_t &input);
+  uint32_t straight32_32(uint32_t &input);
+
+  void keygen(uint64_t key);
 };
+DES::DES(uint64_t key) {
+  round_keys = std::vector<uint64_t>(16, 0);
+  keygen(key);
+}
+
 // definitions
 
 /**
@@ -110,170 +118,283 @@ public:
  *
  * Cipher() function
  */
-std::vector<int> DES::encrypt(std::vector<int> &plainText,
-                              std::vector<std::vector<int>> &roundKeys) {
+uint64_t DES::encrypt(uint64_t &plainText) {
+  uint64_t inBlock = initial_permutation(plainText);
 
-  std::vector<int> inblock = std::move(initial_permutation(plainText));
-
-  std::vector<int> rightBlock =
-      std::move(std::vector<int>(inblock.begin() + 32, inblock.begin() + 64));
-  std::vector<int> leftBlock =
-      std::move(std::vector<int>(inblock.begin(), inblock.begin() + 32));
+  uint32_t lBlock = (inBlock >> 32) & 0x00000000ffffffff;
+  uint32_t rBlock = inBlock & 0x00000000ffffffff;
 
   for (int round = 0; round < 16; round++) {
-    round_mixer(leftBlock, rightBlock, roundKeys[round]);
-    if (round != 16) {
-      std::swap(leftBlock, rightBlock);
+    std::cout << "0x" << std::setfill('0') << std::setw(8) << std::right
+              << std::hex << lBlock << ' ';
+    std::cout << "0x" << std::setfill('0') << std::setw(8) << std::right
+              << std::hex << rBlock << std::endl;
+    round_mixer(lBlock, rBlock, round_keys[round]);
+    if (round != 15) {
+      std::swap(lBlock, rBlock);
     }
   }
-  std::vector<int> output;
-  output.reserve(64);
-  // memory efficient copy
-  output.insert(output.end(), std::make_move_iterator(leftBlock.begin()),
-                std::make_move_iterator(leftBlock.end()));
-  output.insert(output.end(), std::make_move_iterator(rightBlock.begin()),
-                std::make_move_iterator(rightBlock.end()));
+
+  uint64_t outBlock = 0;
+  outBlock |= (lBlock);
+  outBlock <<= 32;
+  outBlock |= (uint64_t)(rBlock);
+
+  outBlock = final_permutation(outBlock);
+  return outBlock;
 }
 /*
- * SBox 48 to 32 in DES
+ * DES decryption
  */
-std::vector<int> DES::substitute(std::vector<int> &input) {
-  std::vector<int> output(32, 0);
-  for (int i = 0; i < 8; i++) {
-    int row = 2 * input[i * 6 + 1] + input[i * 6 + 6];
-    int col = 8 * input[i * 6 + 2] + 4 * input[i * 6 + 3] +
-              2 * input[i * 6 + 4] + input[i * 6 + 5];
+uint64_t DES::decrypt(uint64_t &plainText) {
+  uint64_t inBlock = initial_permutation(plainText);
 
-    int val = sbox[i][row][col];
-    output[i * 4 + 1] = val / 8;
-    output[i * 4 + 2] = val / 4;
-    output[i * 4 + 3] = val / 2;
-    output[i * 4 + 4] = val;
-  }
-  return std::move(output);
-}
-/*
- * bitwise XoR
- */
-std::vector<int> DES::XoR(std::vector<int> &a, std::vector<int> &b) {
-  if (a.size() != b.size()) {
-    std::cout << "XoR error";
+  uint32_t lBlock = (inBlock >> 32) & 0x00000000ffffffff;
+  uint32_t rBlock = inBlock & 0x00000000ffffffff;
+
+  for (int round = 0; round < 16; round++) {
+    round_mixer(lBlock, rBlock, round_keys[15 - round]);
+    if (round != 15) {
+      std::swap(lBlock, rBlock);
+    }
   }
 
-  std::vector<int> c(a.size(), 0);
-  for (int i = 0; i < a.size(); i++) {
-    c[i] = a[i] ^ b[i];
-  }
-  return std::move(c);
-}
-/*
- * 32 bit to 48 bit expansion p box
- */
+  uint64_t outBlock = 0;
+  outBlock |= (lBlock);
+  outBlock <<= 32;
+  outBlock |= (uint64_t)(rBlock);
 
-std::vector<int> DES::expand32_48(std::vector<int> &input) {
-  std::vector<int> output(48, 0);
-
-  for (int i = 0; i < 48; i++) {
-    output[i] = input[expbox[i / 6][i % 6] - 1];
-  }
-  return std::move(output);
-}
-/*
- * 32 bit to 32 bit straight p box
- */
-std::vector<int> DES::straight32_32(std::vector<int> &input) {
-  std::vector<int> output(32, 0);
-
-  if (input.size() != 32) {
-    std::cout << "straight pbox needs a 32 bit input \n";
-  }
-
-  for (int i = 0; i < 32; i++) {
-    output[i] = input[stbox[i / 8][i % 8] - 1];
-  }
-  return std::move(output);
-}
-/*
- * f(i,ki), part of the fiestel cipher in DES
- */
-std::vector<int> DES::round_function(std::vector<int> &input,
-                                     std::vector<int> &roundKey) {
-  std::vector<int> t1(std::move(expand32_48(input)));
-
-  std::vector<int> t2(std::move(XoR(t1, roundKey)));
-
-  std::vector<int> t3(std::move(substitute(t2)));
-
-  return std::move(straight32_32(t3));
+  outBlock = final_permutation(outBlock);
+  return outBlock;
 }
 
 /*
  * Mixer used in each fiestel cipher step
  */
-void DES::round_mixer(std::vector<int> &leftBlock, std::vector<int> &rightBlock,
-                      std::vector<int> &roundKey) {
-  std::vector<int> t1(rightBlock.begin(), rightBlock.end());
-  std::vector<int> t2(std::move(round_function(t1, roundKey)));
-  std::vector<int> t3(std::move(XoR(leftBlock, t2)));
-  rightBlock = std::move(t3);
+void DES::round_mixer(uint32_t &leftBlock, uint32_t &rightBlock,
+                      uint64_t &roundKey) {
+  uint32_t t1 = rightBlock;
+  uint32_t t2 = round_function(t1, roundKey);
+  uint32_t t3 = t2 ^ leftBlock;
+  leftBlock = t3;
+}
+
+/*
+ * f(i,ki), part of the fiestel cipher in DES
+ */
+uint64_t DES::round_function(uint32_t &input, uint64_t &roundKey) {
+  uint64_t t1 = expand32_48(input);
+
+  uint64_t t2 = roundKey ^ t1;
+
+  uint32_t t3 = substitute(t2);
+
+  return straight32_32(t3);
+}
+
+/*
+ * 32 bit to 48 bit expansion p box
+ */
+
+uint64_t DES::expand32_48(uint32_t &input) {
+  uint64_t output = 0;
+
+  for (int i = 0; i < 48; i++) {
+    output <<= 1;
+    output |= (uint64_t)(((input >> (32 - expbox[i / 6][i % 6])) & 0x1));
+    // output[i] = input[expbox[i / 6][i % 6] - 1];
+  }
+  return output;
+}
+/*
+ * SBox 48 to 32 in DES
+ */
+uint32_t DES::substitute(uint64_t &input) {
+  uint32_t output = 0;
+  for (int i = 0; i < 8; i++) {
+    // 84 -> 1000 0100
+    ///     *******
+    unsigned char row =
+        (unsigned char)((input & (0x0000840000000000 >> 6 * i)) >>
+                        (42 - 6 * i));
+
+    row = (row >> 4) | (row & 0x1);
+    // 78-> 0111 1000
+    ///     *******
+    unsigned char col =
+        (unsigned char)((input & (0x0000780000000000 >> 6 * i)) >>
+                        (43 - 6 * i));
+
+    output <<= 4;
+    output |= ((uint32_t)(sbox[i][row][col]) & 0xf);
+  }
+  return output;
+}
+/*
+ * 32 bit to 32 bit straight p box
+ */
+uint32_t DES::straight32_32(uint32_t &input) {
+  uint32_t output = 0;
+
+  for (int i = 0; i < 32; i++) {
+    output <<= 1;
+    output |= ((uint32_t)((input >> (32 - stbox[i / 8][i % 8])) & 0x1));
+    // output[i] = input[expbox[i / 6][i % 6] - 1];
+  }
+  return output;
 }
 
 /*
  * Initial P Box
  */
-std::vector<int> DES::initial_permutation(std::vector<int> &input) {
-  if (input.size() != 8 * 8) {
-    std::cout << "Init box needs 64 bit input";
-  }
-
-  std::vector<int> output(64, 0);
+uint64_t DES::initial_permutation(uint64_t &input) {
+  uint64_t output = 0;
   for (int i = 0; i < 64; i++) {
-    output[fbox[i / 8][i % 8] - 1] = input[i];
+    output <<= 1;
+    output |= (input >> (64 - ibox[i / 8][i % 8])) & 1ULL;
   }
-  return std::move(output);
+  return output;
 }
-uint64_t DES::drop_parity_bits(uint64_t key) {}
 /*
  * Final P box, or inversion of initial P box
  */
-std::vector<int> DES::final_permutation(std::vector<int> &input) {
-  if (input.size() != 8 * 8) {
-    std::cout << "Final box needs 64 bit input";
-  }
-
-  std::vector<int> output;
+uint64_t DES::final_permutation(uint64_t &input) {
+  uint64_t output = 0;
   for (int i = 0; i < 64; i++) {
-    output[ibox[i / 8][i % 8] - 1] = input[i];
+    output <<= 1;
+    output |= (input >> (64 - fbox[i / 8][i % 8])) & 1ULL;
   }
-  return std::move(output);
+  return output;
 }
+
 /*
  * DES Keygen
  */
-std::vector<std::vector<int>> keygen(uint64_t key) {}
+void DES::keygen(uint64_t key) {
+  uint64_t key56 = 0;
+  for (int i = 0; i < 56; i++) {
+    key56 <<= 1;
+    // get the transformed bit, and set it
+    key56 |= (key >> (64 - pdbox[i / 8][i % 8])) & 1ULL;
+  }
+
+  uint32_t leftKey28 = 0;
+  uint32_t rightKey28 = 0;
+
+  leftKey28 = (key56 >> 28) & 0x000000000fffffff;
+  rightKey28 = key56 & 0x000000000fffffff;
+
+  for (int round = 0; round < 16; round++) {
+    if (round == 0 || round == 1 || round == 8 || round == 15) {
+      uint32_t b;
+      b = (leftKey28 >> 27) & 1;
+      leftKey28 <<= 1;
+      leftKey28 |= b;
+      leftKey28 &= 0x000000000fffffff;
+      b = (rightKey28 >> 27) & 1;
+      rightKey28 <<= 1;
+      rightKey28 |= b;
+      rightKey28 &= 0x000000000fffffff;
+
+    } else {
+      uint32_t b;
+      b = (leftKey28 >> 26) & 3;
+      leftKey28 <<= 2;
+      leftKey28 |= b;
+      leftKey28 &= 0x000000000fffffff;
+      b = (rightKey28 >> 26) & 3;
+      rightKey28 <<= 2;
+      rightKey28 |= b;
+      rightKey28 &= 0x000000000fffffff;
+    }
+
+    // combine the keys
+    uint64_t preRoundKey56 = 0;
+    preRoundKey56 |= ((uint64_t)leftKey28);
+    preRoundKey56 <<= 28;
+    preRoundKey56 |= ((uint64_t)rightKey28);
+
+    uint64_t roundKey = 0;
+    for (int i = 0; i < 48; i++) {
+      roundKey <<= 1;
+      // get the transformed bit, and set it
+      roundKey |= (preRoundKey56 >> (56 - kcbox[i / 8][i % 8])) & 1ULL;
+    }
+    round_keys[round] = roundKey;
+  }
+}
 /*
  * Main program, accepts a file with data to be encrypted
  */
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    exit(EXIT_FAILURE);
+  //   uint64_t key = 0xaabb09182736ccdd;
+  uint64_t key = 0xcfbb99162238c019;
+
+  //   uint64_t p = 0x123456abcd132536;
+
+  //   std::string p = "CatLovesFoodYEYEYA";
+  //
+  std::string p;
+  std::cin >> p;
+
+  std::vector<uint64_t> pstream;
+  std::vector<uint64_t> cstream;
+
+  // String to Hex
+  for (int i = 0; i < p.size(); i += 8) {
+    std::string strchunk = p.substr(i, 8);
+
+    if (strchunk.size() != 8) {
+      // padding
+      while (strchunk.size() != 8)
+        strchunk += '*';
+    }
+
+    uint64_t chunk64 = 0;
+    for (int i = 0; i < 8; i++) {
+      uint8_t k = static_cast<uint8_t>(strchunk[i]);
+      chunk64 |= (uint64_t)(k);
+      if (i != 7)
+        chunk64 <<= 8;
+    }
+    pstream.emplace_back(chunk64);
   }
 
-  std::string filename(argv[1]);
-  std::string buffer;
-  std::string input_string = "";
-  std::ifstream input_file(filename);
-  std::ofstream output_file("vignere_decrypt.txt");
+  DES instance(key);
 
-  std::string key = "AABB09182736CCDD";
+  for (auto &p : pstream) {
+    cstream.emplace_back(instance.encrypt(p));
+  }
 
-  while (getline(input_file, buffer)) {
-    input_string += buffer;
+  std::vector<uint64_t> pstream_;
+
+  for (auto &c : cstream) {
+    pstream_.emplace_back(instance.decrypt(c));
   }
-  if (!input_string.size()) {
-    std::cout << "File is empty \n";
-    exit(EXIT_FAILURE);
+
+  // Hex to string
+  //
+  //
+  std::string decrypted;
+  for (auto &p_ : pstream_) {
+    std::string strchunk;
+    for (int i = 0; i < 8; i++) {
+      uint8_t uc = (uint8_t)((p_ >> (56 - i * 8)) & 0xff);
+
+      char c = static_cast<char>(uc);
+      strchunk += c;
+    }
+    decrypted += strchunk;
   }
+
+  std::cout << decrypted << std::endl;
+  //   uint64_t c = instance.encrypt(p);
+
+  //   std::cout << std::hex << c << std::endl;
+
+  //   uint64_t p_ = instance.decrypt(c);
+
+  //   std::cout << std::hex << p_ << std::endl;
 
   return 0;
 }
